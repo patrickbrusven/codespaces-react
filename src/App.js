65f8b0b26854497b16e5cd5984559122c094361a
Patrick Brusven <84103244+patrickbrusven@users.noreply.github.com>
2023-02-09 22:01:41 +0000

    WIP hero design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.scss";
 import "./Normalize.css";
 import HeroSection from "./HeroSection";
 
--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -4,9 +4,18 @@ function HeroSection() {
   return (
     <>
       <div className="hero-card">
-        <CornerBlobs wrapperClass={"corner-blobs"}></CornerBlobs>
-        {/* <h1>Patrick Brusven</h1>
-        <h2>Software Developer</h2> */}
+        <CornerBlobs wrapperClass={"hero-card__background"}></CornerBlobs>
+        <div className="hero-card__content">
+          <p className="highlight-text">Hi, my name is</p>
+          <h1 className="heading-text">Patrick Brusven.</h1>
+          <h2 className="subheading-text">
+            I'm a Front-End leaning Full-Stack Developer.
+          </h2>
+          <p className="content-text">
+            Technology amazes me every day and is why I transitioned form retail
+            managment to engineering.
+          </p>
+        </div>
       </div>
     </>
   );
